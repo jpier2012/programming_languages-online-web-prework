4e4db85505f611e7233f1fd9b466023ac1d9d94c
James Pier <jpier2012@gmail.com>
2019-04-11 12:59:10 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,89 @@
-def reformat_languages(languages)
-  # your code here
+require 'pry'
+
+def language_hash
+  language_hash = {
+    :oo => {
+      :ruby => {
+        :type => "interpreted"
+      },
+      :javascript => {
+        :type => "interpreted"
+      },
+      :python => {
+        :type => "interpreted"
+      },
+      :java => {
+        :type => "compiled"
+      }
+    },
+    :functional => {
+      :clojure => {
+        :type => "compiled"
+      },
+      :erlang => {
+        :type => "compiled"
+      },
+      :scala => {
+        :type => "compiled"
+      },
+      :javascript => {
+        :type => "interpreted"
+      }
+    }
+  }
 end
+
+#####
+#####
+
+def lang_array
+  langs = []
+
+  language_hash.each {|style, language_name|
+  # :oo, :functional
+    langs << language_name.keys
+  }
+
+  langs.flatten!.uniq!
+end
+
+#####
+#####
+
+def type_array
+  type_array = []
+
+  language_hash.each {|style, language_name|
+  # style = :oo, :functional
+    language_name.each {|lang, type|
+    # lang = :ruby, :javascript, :python, etc.
+      type_array << type
+    }
+  }
+
+  type_array
+end
+
+#####
+#####
+
+def reformat_languages(languages)
+  new_hash = {}
+  lang_style = []
+
+  new_hash = lang_array.zip(type_array)
+
+  new_hash.each {|language_name, info|
+  # language_name = :ruby, :javascript, :clojure, etc etc
+      language_hash.each {|style, language_name|
+        # :oo, :functional
+      langs << language_name.keys
+    }
+  }
+  new_hash
+end
+
+#####
+#####
+
+reformat_languages(language_hash)
---
+++ test.rb
@@ -0,0 +1,59 @@
+require 'pry'
+
+def language_hash
+  language_hash = {
+    :oo => {
+      :ruby => {
+        :type => "interpreted"
+      },
+      :javascript => {
+        :type => "interpreted"
+      },
+      :python => {
+        :type => "interpreted"
+      },
+      :java => {
+        :type => "compiled"
+      }
+    },
+    :functional => {
+      :clojure => {
+        :type => "compiled"
+      },
+      :erlang => {
+        :type => "compiled"
+      },
+      :scala => {
+        :type => "compiled"
+      },
+      :javascript => {
+        :type => "interpreted"
+      }
+    }
+  }
+end
+
+#####
+#####
+
+def reformat_languages(languages)
+  new_hash = {}
+  style_array = []
+  languages.each {|style, language|
+  # style = :oo, :functional
+  style_array = style
+    language.each {|language_name, info|
+    # language_name = :ruby, :javascript, :clojure, etc etc
+      info << {:style => style_array}
+
+        new_hash[language_name] = info
+      binding.pry
+    }
+  }
+  new_hash
+end
+
+#####
+#####
+
+puts reformat_languages(language_hash)
